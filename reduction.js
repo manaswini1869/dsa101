05c158447d862d94cd750358f7307cc8040f9729
Manaswini1869 <goginenim18069@gmail.com>
2024-12-10 00:34:26 -0500

    feat(code): redunction array

---
+++ reduction.js
@@ -0,0 +1,15 @@
+function findTotalCost(input) {
+    let total_cost = 0
+    while(input.length > 1) {
+        input.sort((a,b) => (a-b))
+        let min_element = input.shift();
+        let max_element = input.pop();
+        const cost = Math.ceil((min_element+max_element) / (max_element-min_element+1));
+        total_cost += cost;
+        input.push(min_element+max_element);
+    }
+    return total_cost
+}
+
+
+console.log(findTotalCost([3, 5, 2, 1, 9, 6]));
