8034fbe42c5f34b98e0c8d0f5b213e8150158866
Manaswini Gogineni <mgogineni@wisc.edu>
2025-01-23 15:19:21 -0600

    subtree

--- trees/sameTree.py
+++ trees/sameTree.py
@@ -8,17 +8,16 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
 
-        def traverse(r1, r2):
-            if not r1 and not r2:
-                return True
-            if not r1 or not r2:
-                return False
-            if r1.val != r2.val:
-                return False
-            left = traverse(r1.left, r2.left)
-            right = traverse(r1.right, r2.right)
-            if left and right:
-                return True
-            else:
-                return False
-        return traverse(p, q)
+        # def traverse(r1, r2):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+        if left and right:
+            return True
+        else:
+            return False
