29c1342240716b4897c89386fe578838ef33e01c
Manaswini Gogineni <goginenim18069@gmail.com>
2025-07-31 00:25:46 -0700

    daily

---
+++ dailyLeetcode/Bitwise ORs of Subarrays.py
@@ -0,0 +1,8 @@
+class Solution:
+    def subarrayBitwiseORs(self, arr: List[int]) -> int:
+        res = set()
+        curr = set()
+        for num in arr:
+            curr = {num | x for x in curr} | {num}
+            res |= curr
+        return len(res)
