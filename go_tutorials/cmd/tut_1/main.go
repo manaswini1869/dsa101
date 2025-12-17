4d372578e0e18082de7d323077d04d12051dd361
Manaswini Gogineni <goginenim18069@gmail.com>
2025-12-17 10:49:46 -0600

    daily

--- go_tutorials/cmd/tut_1/main.go
+++ go_tutorials/cmd/tut_1/main.go
@@ -1,32 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var printMessageParam string = "Hello, World!"
-	printMessage(printMessageParam)
 
-	quotient, remainder, err := intDivision(10, 5)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	} else if remainder == 0 {
-		fmt.Printf("Quotient %d", quotient)
-	} else {
-		fmt.Printf("Quotient: %d, Remainder: %d\n", quotient, remainder)
-	}
-
-}
-
-func printMessage(printMessage string) {
-	fmt.Println(printMessage)
-}
-
-func intDivision(a int, b int) (int, int, error) {
-	var err error
-	if b == 0 {
-		err = fmt.Errorf("division by zero is not allowed")
-		return 0, 0, err
-	}
-	return a / b, a % b, err
 }
