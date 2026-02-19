8a2c904bf4ed21ddc4bc631003cc8fa404d5da55
Manaswini Gogineni <goginenim18069@gmial.com>
2026-02-18 21:40:32 -0500

    daily

---
+++ dailyLeetcode/Count Binary Substrings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        prev_group = 0
+        curr_group = 1
+        count = 0
+
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                curr_group += 1
+            else:
+                count += min(prev_group, curr_group)
+                prev_group = curr_group
+                curr_group = 1
+
+        count += min(prev_group, curr_group)
+
+        return count
