12e450a05d94fc33d74b5fddb7e4b00e0617b95c
Manaswini Gogineni <goginenim18069@gmial.com>
2026-05-28 22:29:08 -0400

    daily

---
+++ dailyLeetcode/Minimum Element After Replacement With Digit Sum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+
+        ans = float("inf")
+        for num in nums:
+            curr = 0
+            while num:
+                rem = num % 10
+                curr += rem
+                num = num // 10
+            ans = min(ans, curr)
+        return ans
+
+
+        
